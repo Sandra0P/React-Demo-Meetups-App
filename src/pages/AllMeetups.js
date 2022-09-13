5494435016333f3273bc9f6e4f9df54564c669e2
Sandra <sandrapopa8@gmail.com>
2022-09-13 21:04:38 +0300

    Added data fetching for main AllMeetups page

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,57 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import { useState, useEffect } from "react";
 
 //map() iterates over an array and is used for rendering a list of data
 function AllMeetupsPage() {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); //use state in order to decide when should we load new info from the database
+  //start in a loading state, set it to false once we have the data
+
+  useEffect(() => {
+    //useEffect  runs the given function only under given conditions
+    setIsLoading(true);
+    fetch("https://react-demo-app-99b29-default-rtdb.firebaseio.com/meetups.json", {
+      method: "GET", //default method
+      headers: {
+        "Access-Control-Allow-Origin": "localhost:3000",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+      },
+    })
+      .then((response) => {
+        return response.json(); // after fetch is done, use its response variable to get the data stored in the databse
+      })
+      .then((data) => {
+        //response.json() returns the data in json form so we need another function to parse it
+        const meetups = [];
+        for (const ID in data) {
+          //for meetup in database
+          const meetup = {
+            id: ID, //unique id of each database
+            ...data[ID], //copy all the values stored in each database meetup into local meetup object
+          };
+          meetups.push(meetup); //append each meetup in meetups
+        }
+        console.log(meetups);
+        setIsLoading(false); //set loading state to false
+        setLoadedMeetups(meetups);
+      });
+  }, []); //dependencies array - react checks variables added to array and compares them to their values the last time this function executed
+
+  if (isLoading) {
+    //logic explanation: JS loads html data in this function without waiting for fetch to finish working.
+    //thats why we will use isLoading state to output different html code while fetch is working
+    //and we output the actual loaded meetups only when isLoading registers as false
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1> All Meetups</h1>
-      <MeetupList meetups = {DUMMY_DATA}/>
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
