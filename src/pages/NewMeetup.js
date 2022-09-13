5a3cd3006a4d57d0394ea52e90e6799762900a91
Sandra <sandrapopa8@gmail.com>
2022-09-13 20:12:12 +0300

    added programmatic navigation - once user is done filling in the form program automatically navigates back to the start page. used useHistory hook for this

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,22 +1,25 @@
+import {useHistory} from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
-  function addMeetupHandler(meetupData) {
-    //save meetupData in backend server
-    //add "folder" in database by adding the /foldername in the end of the url
-    //firebase requires that we add .json to the end of the url
-    fetch(
-      "https://react-demo-app-99b29-default-rtdb.firebaseio.com/meetups.json",
-      {
-        method: 'POST',
-        body: JSON.stringify(meetupData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      } 
-    );
-    //method defines the request type (POST = upload), body contains the content to save
-    //content needs to be sent in JSON format so we use JSON.stringify to convert it that way 
-  }
+    const history = useHistory();
+    function addMeetupHandler(meetupData) {
+        //save meetupData in backend server
+        //add "folder" in database by adding the /foldername in the end of the url
+        //firebase requires that we add .json to the end of the url
+        fetch(
+        "https://react-demo-app-99b29-default-rtdb.firebaseio.com/meetups.json",
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        } 
+        ).then(() => 
+            history.replace('/')); //navigating away once the request succeeded + going back won't take us back to the form
+        //method defines the request type (POST = upload), body contains the content to save
+        //content needs to be sent in JSON format so we use JSON.stringify to convert it that way 
+    }
   return (
     <section>
       <h1> Add new Meetup</h1>
